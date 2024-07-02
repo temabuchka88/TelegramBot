0f6316d9f6df0b3cdec3e421806408b86274bc5f
artem <asd@example.com>
2024-07-02 21:31:54 +0300

    fix prise 2

--- handlers/user/prise_list.py
+++ handlers/user/prise_list.py
@@ -1,5 +1,5 @@
 from aiogram import Router, F
-from aiogram.types import Message, InputFile 
+from aiogram.types import Message, FSInputFile 
 from keyboards.user.main_menu import all_steps_button
 import os
 
@@ -10,5 +10,5 @@ prise_path = os.path.join(current_dir, 'prise.jpg')
 
 @router.message(F.text == "Прайс-лист")
 async def show_prise_list(message: Message):
-    prise = InputFile(prise_path)
+    prise = FSInputFile(prise_path)
     await message.answer_photo(prise, reply_markup=all_steps_button())
